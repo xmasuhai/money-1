1764b86e16dc0c8c2d7d97d15427f031e1def3d3
xmasuhai <xmasuhai@163.com>
2021-01-16 23:20:06 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, {RouteConfig} from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
     path: '/statistics',
     component: Statistics
   },
+  {
+    path: '*',
+    component: NotFound
+  }
 ];
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    当前路径不存在，请检查网址是否正确
+    <br>
+    <router-link to="/">返回首页</router-link>
+    <br>
+    <a href="/#">返回首页</a>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
