b8fc919c4d1e30e99e3afa89ea7b9d892819947b
xmasuhai <xmasuhai@163.com>
2021-01-17 00:11:03 +0800

    Money 页面布局

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~@/assets/style/global.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
-    Money.vue
-    <Nav></Nav>
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+    </div>
+    <Nav/>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: "Money",
+  name: 'Money',
 };
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  .content {
+    border: 1px solid cornflowerblue;
+    flex-grow: 1;
+    overflow: auto;
+    }
+  }
 </style>
