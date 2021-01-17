79d215bd4dc57c934f05fc2f08445fce32658926
xmasuhai <xmasuhai@163.com>
2021-01-17 11:44:17 +0800

    创建 Icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,46 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="'#'+name"/>
+  </svg>
+</template>
+
+<script lang="ts">
+/*
+import Vue from "vue"
+import Component from "vue-class-component"
+*/
+
+/*
+const importAll = (requireContext) => {
+  requireContext.keys().forEach(requireContext)
+}
+*/
+
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => {
+  requireContext.keys().forEach(requireContext);
+};
+// 指定目录 只能用相对路径 不支持@别名路径
+// 使用importAll加载所有的svg
+importAll(require.context('../assets/icons/', true, /\.svg$/));
+
+// let importAll = (requireContext: __WebpackModuleApi.requireContext) => requireContext.keys().forEach(requireContext)
+try {
+  importAll(require.context('../assets/icons/', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
+export default {
+  name: 'Icon',
+  props: ['name']
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 2em; height: 2em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  }
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,33 @@
 <template>
   <div>
     <nav>
-      <svg>
-        <use xlink:href="#bills"/>
-      </svg>
-      <router-link to="/labels">标签</router-link>
+      <router-link to="/labels">
+        <Icon name="bills"/>
+        标签
+      </router-link>
       |
-      <router-link to="/money">记账</router-link>
+      <router-link to="/money">
+        <Icon name="account"/>
+        记账
+      </router-link>
       |
-      <router-link to="/statistics">统计</router-link>
+      <router-link to="/statistics">
+        <Icon name="statistics"/>
+        统计
+      </router-link>
     </nav>
   </div>
 </template>
 
 <script lang="ts">
 import x from '@/assets/icons/bills.svg';
+import Icon from '@/components/Icon.vue';
 
 console.log(x);
 
 export default {
-  name: 'Nav'
+  name: 'Nav',
+  components: {Icon}
 };
 </script>
 
