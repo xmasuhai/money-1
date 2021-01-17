4996c6c76695a8d1fba94669c04d7b7323dcabd2
xmasuhai <xmasuhai@163.com>
2021-01-17 10:38:38 +0800

    add svg-sprite-loader

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <nav>
-      <router-link to="/money">记账</router-link>
-      |
+      <svg>
+        <use xlink:href="#bills"/>
+      </svg>
       <router-link to="/labels">标签</router-link>
+      |
+      <router-link to="/money">记账</router-link>
       |
       <router-link to="/statistics">统计</router-link>
     </nav>
@@ -11,6 +14,10 @@
 </template>
 
 <script lang="ts">
+import x from '@/assets/icons/bills.svg';
+
+console.log(x);
+
 export default {
   name: 'Nav'
 };
