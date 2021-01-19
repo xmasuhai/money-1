e18e2172c8d40aca50350a4a710dd719babc1d32
xmasuhai <xmasuhai@163.com>
2021-01-20 07:28:45 +0800

    解决 SVG 自带填充色 fill 属性

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="bills"/>
       标签
     </router-link>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="account"/>
       记账
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -42,6 +42,9 @@ nav {
       width: 32px;
       height: 32px;
       }
+    }
+  > .item.selected{
+    color: orangered;
     }
   }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,14 @@ module.exports = {
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons') // 确定目录
-    config.module.uses.clear()
+    config.module
       .rule('svg-sprite')
       .test(/\.svg$/)// .test(/\.(svg)(\?.*)?$/)
       .include.add(dir).end() // 指定 仅包含 icons 的目录
       .use('svg-sprite-loader').loader('svg-sprite-loader')
       .options({extract: false}).end() // 不解析出文件
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options =>({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
 
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir)
